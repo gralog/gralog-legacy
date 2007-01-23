d076a9c1579b737c29ab3af953362d05437fc8bd
lukasmoll <>
2007-01-23 19:40:58 +0000

    *** empty log message ***

--- NBA_isEmpty/src/de/hu/NBA_isEmpty/alg/NBA_isEmptyAlgorithm.java
+++ NBA_isEmpty/src/de/hu/NBA_isEmpty/alg/NBA_isEmptyAlgorithm.java
@@ -31,7 +31,7 @@ public class NBA_isEmptyAlgorithm<V extends LabeledGraphVertex,E extends Default
 		
 		while (it.hasNext()) {
 			Set<V> s = it.next();
-			V nodeOfSet = (V)s.toArray()[0];
+			V nodeOfSet = s.iterator().next();
 			
 //			every strong connected set, with only one element or which is out of reach of the startVertex is ignored
 			if ((s.size() == 1) || (DijkstraShortestPath.findPathBetween(gameGraph, startVertex, nodeOfSet) == null))
