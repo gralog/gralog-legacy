182eee4070d53208c1e136431279db8a8e412f21
Thomas <ThomasKarbe@gmx.de>
2015-01-18 20:01:28 +0100

    added log messages, formatted code

--- app/src/de/hu/gralog/gui/data/Plugin.java
+++ app/src/de/hu/gralog/gui/data/Plugin.java
@@ -29,16 +29,19 @@ import de.hu.gralog.structure.StructureTypeInfo;
 public class Plugin {
 
 	static final Logger LOGGER = LoggerFactory.getLogger(Plugin.class);
+	static final Logger AILOGGER = LoggerFactory.getLogger(AlgorithmInfo.class);
 
 	public class AlgorithmInfo {
-		private String name;
-		private String path;
+		private final String name;
+		private final String path;
 		private String description;
-		private Class<? extends Algorithm> type;
+		private final Class<? extends Algorithm> type;
 		private Algorithm algorithm;
 
-		public AlgorithmInfo(String name, Class<? extends Algorithm> type)
-				throws UserException {
+		public AlgorithmInfo(final String name,
+				final Class<? extends Algorithm> type) throws UserException {
+			AILOGGER.debug("entering constructor with name {} and type {}",
+					name == null ? "null" : name, type == null ? "null" : type);
 			this.path = name;
 			this.name = name;
 
@@ -46,212 +49,261 @@ public class Plugin {
 			 * if ( path.indexOf( '.' ) != -1 ) this.name = name.substring(
 			 * name.lastIndexOf( '.' ) + 1 ); else this.name = name;
 			 */this.type = type;
+			AILOGGER.debug("exiting constructor");
 		}
 
 		public String getName() {
-			return name;
+			return this.name;
 		}
 
 		public Class<? extends Algorithm> getType() {
-			return type;
+			return this.type;
 		}
 
 		public String getDescription() {
-			return description;
+			return this.description;
 		}
 
 		public Algorithm getAlgorithm() {
-			return algorithm;
+			return this.algorithm;
 		}
 
 		public String getPath() {
-			return Plugin.this.name + "." + path;
+			return Plugin.this.name + "." + this.path;
 		}
 
+		@Override
 		public String toString() {
-			return name;
+			return this.name;
 		}
 
 		void loadAlgorithm() throws UserException {
+			AILOGGER.debug("entering loadAlgorithm()");
 			try {
-				this.algorithm = type.newInstance();
+				this.algorithm = this.type.newInstance();
 			}
-			catch (InstantiationException e) {
+			catch (final InstantiationException e) {
 				throw new UserException("unable to load plugin", e);
 			}
-			catch (IllegalAccessException e) {
+			catch (final IllegalAccessException e) {
 				throw new UserException("unable to load plugin", e);
 			}
 
 			try {
-				description = Introspector.getBeanInfo(type)
+				AILOGGER.debug("getting description from beaninfo for type: {}", type==null?"null":type);
+				this.description = Introspector.getBeanInfo(this.type)
 						.getBeanDescriptor().getShortDescription();
 			}
-			catch (Throwable e) {
+			catch (final Throwable e) {
 				throw new UserException("unable to load plugin", e);
 			}
+			AILOGGER.debug("exiting loadAlgorithm()");
 		}
 	}
 
 	private String name;
 	private String description;
-	private ArrayList<Class<StructureTypeInfo>> structureTypeClasses = new ArrayList<Class<StructureTypeInfo>>();
+	private final ArrayList<Class<StructureTypeInfo>> structureTypeClasses = new ArrayList<Class<StructureTypeInfo>>();
 	private ArrayList<StructureTypeInfo> structureTypeInfos = null;
-	private ArrayList<AlgorithmInfo> algorithms = new ArrayList<AlgorithmInfo>();
+	private final ArrayList<AlgorithmInfo> algorithms = new ArrayList<AlgorithmInfo>();
 	private boolean algorithmsLoaded = false;
 
-	public Plugin(File file) throws UserException {
-		LOGGER.debug("entering contructor with file: {}", file);
+	public Plugin(final File file) throws UserException {
+		LOGGER.debug("entering contructor with file: {}", file == null ? "null"
+				: file);
 		try {
-			if (!file.exists())
+			if (!file.exists()) {
 				throw new UserException("unable to load plugin " + file,
 						"The file does not exist");
-			if (!file.canRead())
+			}
+			if (!file.canRead()) {
 				throw new UserException("unable to load plugin " + file,
 						"The file is not readable");
-			if (!file.isFile())
+			}
+			if (!file.isFile()) {
 				throw new UserException("unable to load plugin " + file,
 						"The file is no file");
+			}
 
-			JarFile jarFile = new JarFile(file);
+			final JarFile jarFile = new JarFile(file);
 
-			ZipEntry entry = jarFile.getEntry("plugin.config");
-			if (entry == null)
+			final ZipEntry entry = jarFile.getEntry("plugin.config");
+			if (entry == null) {
+				jarFile.close();
 				throw new UserException("unable to load plugin " + file,
 						"The file contains no plugin.config");
+			}
 
 			MainPad.getInstance().readJarFile(jarFile);
 
-			loadSettings(jarFile.getInputStream(entry));
-			LOGGER.debug("exiting constructor");
+			this.loadSettings(jarFile.getInputStream(entry));
 		}
-		catch (IOException e) {
+		catch (final IOException e) {
 			throw new UserException("unable to load jarFile " + file, e);
 		}
-		catch (JDOMException e) {
+		catch (final JDOMException e) {
 			throw new UserException("unable to load jarFile " + file, e);
 		}
+		LOGGER.debug("exiting constructor");
 	}
 
-	public Plugin(InputStream in) throws UserException {
+	public Plugin(final InputStream in) throws UserException {
+		LOGGER.debug("entering contructor with InputStream: {}",
+				in == null ? "null" : in);
 		try {
-			loadSettings(in);
+			this.loadSettings(in);
 		}
-		catch (JDOMException e) {
+		catch (final JDOMException e) {
 			throw new UserException("unable to load plugin ", e);
 		}
-		catch (IOException e) {
+		catch (final IOException e) {
 			throw new UserException("unable to load plugin ", e);
 		}
+		LOGGER.debug("exiting constructor");
 	}
 
-	protected void loadSettings(InputStream in) throws JDOMException,
+	protected void loadSettings(final InputStream in) throws JDOMException,
 			IOException, UserException {
-		SAXBuilder parser = new SAXBuilder();
-		loadSettings(parser.build(in).getRootElement());
+		LOGGER.debug("entering loadSettings(in) with InputStream: {}",
+				in == null ? "null" : in);
+		final SAXBuilder parser = new SAXBuilder();
+		this.loadSettings(parser.build(in).getRootElement());
+		LOGGER.debug("exiting loadSettings(in)");
 	}
 
-	protected void loadSettings(Element root) throws UserException {
-		name = root.getAttributeValue("name");
-		Element description = root.getChild("description");
-		if (description != null)
-			loadDescription(description);
-		Element graphs = root.getChild("structures");
-		if (graphs != null)
-			loadGraphsSettings(graphs);
-		Element algorithms = root.getChild("algorithms");
-		if (algorithms != null)
-			loadAlgorithmsSettings(algorithms);
+	protected void loadSettings(final Element root) throws UserException {
+		LOGGER.debug("entering loadSettings(root) with Element: {}",
+				root == null ? "null" : root);
+		this.name = root.getAttributeValue("name");
+		final Element description = root.getChild("description");
+		if (description != null) {
+			this.loadDescription(description);
+		}
+		final Element graphs = root.getChild("structures");
+		if (graphs != null) {
+			this.loadGraphsSettings(graphs);
+		}
+		final Element algorithms = root.getChild("algorithms");
+		if (algorithms != null) {
+			this.loadAlgorithmsSettings(algorithms);
+		}
+		LOGGER.debug("exiting loadSettings(root)");
 	}
 
-	protected void loadDescription(Element description) throws UserException {
+	protected void loadDescription(final Element description)
+			throws UserException {
 		this.description = description.getTextTrim();
 	}
 
-	protected void loadGraphsSettings(Element graphs) throws UserException {
-		for (Element child : (List<Element>) graphs.getChildren())
-			loadGraphSettings(child);
+	protected void loadGraphsSettings(final Element graphs)
+			throws UserException {
+		for (final Element child : (List<Element>) graphs.getChildren()) {
+			this.loadGraphSettings(child);
+		}
 	}
 
-	protected void loadGraphSettings(Element graph) throws UserException {
-		String typeInfoClassName = graph.getAttributeValue("typeInfoClass");
-		if (typeInfoClassName == null)
+	protected void loadGraphSettings(final Element graph) throws UserException {
+		LOGGER.debug("entering loadGraphSettings(graph) with Element: {}",
+				graph == null ? "null" : graph);
+		final String typeInfoClassName = graph
+				.getAttributeValue("typeInfoClass");
+		if (typeInfoClassName == null) {
 			throw new UserException("unable to load settings",
 					"structure-Element has no typeInfoClassattribute");
+		}
 		try {
-			structureTypeClasses.add((Class<StructureTypeInfo>) MainPad
+			this.structureTypeClasses.add((Class<StructureTypeInfo>) MainPad
 					.getInstance().getJarLoader().loadClass(typeInfoClassName));
 		}
-		catch (ClassNotFoundException e) {
+		catch (final ClassNotFoundException e) {
 			throw new UserException("unable to load plugin", e);
 		}
+		LOGGER.debug("exiting loadGraphSettings() with Element");
 	}
 
-	protected void loadAlgorithmsSettings(Element algorithms)
+	protected void loadAlgorithmsSettings(final Element algorithms)
 			throws UserException {
-		for (Element child : (List<Element>) algorithms.getChildren())
-			loadAlgorithmSettings(child);
+		LOGGER.debug(
+				"entering loadAlgorithmsSettings(algorithms) with Element: {}",
+				algorithms == null ? "null" : algorithms);
+		for (final Element child : (List<Element>) algorithms.getChildren()) {
+			this.loadAlgorithmSettings(child);
+		}
+		LOGGER.debug("exiting loadAlgorithmsSettings(algorithms)");
 	}
 
-	protected void loadAlgorithmSettings(Element algorithm)
+	protected void loadAlgorithmSettings(final Element algorithm)
 			throws UserException {
-		String name = algorithm.getAttributeValue("name");
-		String typeName = algorithm.getAttributeValue("class");
-		if (name == null || typeName == null)
+		LOGGER.debug(
+				"entering loadAlgorithmSettings(algorithm) with Element: {}",
+				this.algorithms == null ? "null" : this.algorithms);
+		final String name = algorithm.getAttributeValue("name");
+		final String typeName = algorithm.getAttributeValue("class");
+		if (name == null || typeName == null) {
 			throw new UserException("unable to load plugin",
 					"algorithm has no type or name attribute");
+		}
 
 		try {
-			Class<? extends Algorithm> type = (Class<? extends Algorithm>) MainPad
+			final Class<? extends Algorithm> type = (Class<? extends Algorithm>) MainPad
 					.getInstance().getJarLoader().loadClass(typeName);
-			algorithms.add(new AlgorithmInfo(name, type));
+			this.algorithms.add(new AlgorithmInfo(name, type));
 		}
-		catch (ClassNotFoundException e) {
+		catch (final ClassNotFoundException e) {
 			throw new UserException("unable to load plugin", e);
 		}
+		LOGGER.debug("exiting loadAlgorithmSettings(algorithms)");
 	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	public String getDescription() {
-		return description;
+		return this.description;
 	}
 
 	public ArrayList<StructureTypeInfo> getStructureTypeClasses() {
-		if (structureTypeInfos == null) {
-			structureTypeInfos = new ArrayList<StructureTypeInfo>();
-			for (Class<StructureTypeInfo> typeInfoClass : structureTypeClasses) {
+		LOGGER.debug("entering getStructureTypeClasses()");
+		if (this.structureTypeInfos == null) {
+			this.structureTypeInfos = new ArrayList<StructureTypeInfo>();
+			for (final Class<StructureTypeInfo> typeInfoClass : this.structureTypeClasses) {
 				try {
-					structureTypeInfos.add(typeInfoClass.newInstance());
+					this.structureTypeInfos.add(typeInfoClass.newInstance());
 				}
-				catch (InstantiationException e) {
+				catch (final InstantiationException e) {
 					MainPad.getInstance().handleUserException(
 							new UserException("unable to load plugin", e));
 				}
-				catch (IllegalAccessException e) {
+				catch (final IllegalAccessException e) {
 					MainPad.getInstance().handleUserException(
 							new UserException("unable to load plugin", e));
 				}
 			}
 		}
 
-		return structureTypeInfos;
+		LOGGER.debug("exiting getStructureTypeClasses() with return value: {}",
+				this.structureTypeInfos == null ? "null"
+						: this.structureTypeInfos);
+
+		return this.structureTypeInfos;
 	}
 
 	public ArrayList<AlgorithmInfo> getAlgorithms() {
-		if (!algorithmsLoaded) {
-			for (AlgorithmInfo info : algorithms) {
+		LOGGER.debug("entering getAlgorithms()");
+		if (!this.algorithmsLoaded) {
+			for (final AlgorithmInfo info : this.algorithms) {
 				try {
 					info.loadAlgorithm();
 				}
-				catch (UserException e) {
+				catch (final UserException e) {
 					MainPad.getInstance().handleUserException(e);
 				}
 			}
-			algorithmsLoaded = true;
+			this.algorithmsLoaded = true;
 		}
-		return algorithms;
+		LOGGER.debug("exiting getAlgorithms() with return value: {}",
+				this.algorithms == null ? "null" : this.algorithms);
+		return this.algorithms;
 	}
 }
